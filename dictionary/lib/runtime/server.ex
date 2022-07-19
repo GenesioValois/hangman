fbae7993874bca568ec7d536db091a95754e0edf
Genesio Valois <genas96@gmail.com>
2022-07-19 18:52:00 +0100

    test: Crash th application visualize the surpervisor

--- dictionary/lib/runtime/server.ex
+++ dictionary/lib/runtime/server.ex
@@ -11,7 +11,12 @@ defmodule Dictionary.Runtime.Server do
     Agent.start_link(&WordList.word_list/0, name: @me)
   end
 
+  # randomly crash the app to validate surpervisor
   def random_word() do
+    if :rand.uniform() < 0.33 do
+      Agent.get(@me, fn _ -> exit(:boom) end)
+    end
+
     Agent.get(@me, &WordList.random_word/1)
   end
 end
