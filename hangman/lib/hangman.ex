84b6b4c69e447749445089edb07260cf41c6d934
Genesio Valois <genas96@gmail.com>
2022-08-04 12:13:01 +0100

    feat: Use Application to handle new games

--- hangman/lib/hangman.ex
+++ hangman/lib/hangman.ex
@@ -1,24 +1,23 @@
 defmodule Hangman do
-
+  alias Hangman.Runtime.Application
   alias Hangman.Runtime.Server
   alias Hangman.Type
 
-  @opaque game :: Server.t
+  @opaque game :: Server.t()
 
   @spec new_game :: game
   def new_game do
-    { :ok, pid } = Server.start_link()
+    {:ok, pid} = Application.start_game()
     pid
   end
 
-  @spec make_move(game, String.t) :: { game, Type.tally }
+  @spec make_move(game, String.t()) :: Type.tally()
   def make_move(game, guess) do
-    GenServer.call(game, { :make_move, guess })
+    GenServer.call(game, {:make_move, guess})
   end
 
   @spec tally(game) :: Type.tally()
   def tally(game) do
-    GenServer.call(game, { :tally })
+    GenServer.call(game, {:tally})
   end
-
 end
